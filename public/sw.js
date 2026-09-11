a52bb89e024174e3ff2a4dfe8d08ba3a7f5fb654
Otero <oterolopezandrikaxel@gmail.com>
2026-09-10 18:47:21 -0600

    Bump service worker version to clear cache

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.02"
+const VERSION = "1.03"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
