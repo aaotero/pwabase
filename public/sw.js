bc49433648f246eb39d31572df389ca91e77ec42
Otero <oterolopezandrikaxel@gmail.com>
2026-09-17 18:15:54 -0600

    Bump SW version to 1.07 to force cache update

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.06"
+const VERSION = "1.07"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
