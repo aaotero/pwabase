d9963b296bf2a0925d0c44e0d77065a5f3df51e3
Otero <oterolopezandrikaxel@gmail.com>
2026-09-17 18:18:49 -0600

    Force manifest cache bypass with query parameter

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.07"
+const VERSION = "1.08"
 
 /** Nombre del archivo de cache. */
 const CACHE = "ejemploPWA"
@@ -39,7 +39,7 @@ const ARCHIVOS = [
   "img/VerticalScreenshot_DigitalSquad.png",
   "index.html",
   "js/registraServiceWorker.js",
-  "site.webmanifest",
+  "site.webmanifest?v=2",
   "/"
 ]
 
